f19431ff56f1439604301d1e97b466830f173108
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-03-24 13:53:19 +0330

    button update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ className }) {
   return (
     <button
-      className={`${className} px-7 py-4 text-white rounded-full text-base lg:text-lg font-bold leading-7 cursor-pointer`}
+      className={`${className} px-6 py-2 lg:px-8 lg:py-3 text-white rounded-full text-base lg:text-lg font-bold leading-7 cursor-pointer`}
     >
       Get Started
     </button>
